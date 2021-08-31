adce6342086d6787da433e102260bb55194b9f4e
OloloevReal <nickey.n@gmail.com>
2021-09-01 02:13:50 +0300

    Lab_2_6 done
    Lab_2_5 done

--- lab_2_5.cs
+++ lab_2_5.cs
@@ -3,12 +3,39 @@ using System;
 namespace Labs_2
 {
     class Lab_2_5: ILab{
+        private const string Message = "Не удается определить середину массива, кол-во колонок четное.";
+        private int[,] items;
+        private int col = 5;
+        private int row = 6;
+
+        public Lab_2_5(){
+            items = new int[col, row];
+        }
         public override void Do(){
+            Utils.FillArray(this.items);
+            Console.WriteLine("Source values: ");
+            Utils.PrintArray(this.items);
+
+            if (col % 2 == 0) {
+               throw new Exception(Message);
+            }
+
+            for(int i = 0; i < col / 2; i++){
+                for(int j = 0; j < row; j++){
+                    int tmp = items[i, j];
+                    items[i, j] = items[col - 1 - i, j];
+                    items[col - 1 - i, j] = tmp;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Result values: ");
+            Utils.PrintArray(this.items);
             Console.WriteLine();
         }
 
         public override string Description(){
-           return @"2.5. В двухмерном массиве целых чисел поменять местами столбцы, симметричные относительно середины массива (вертикальнойлинии).";
+           return @"2.5. В двухмерном массиве целых чисел поменять местами столбцы, симметричные относительно середины массива (вертикальной линии).";
         }
     }
 
--- lab_2_6.cs
+++ lab_2_6.cs
@@ -3,7 +3,35 @@ using System;
 namespace Labs_2
 {
     class Lab_2_6: ILab{
+
+        private const string Message = "Не удается определить середину массива, кол-во рядов четное.";
+        private int[,] items;
+        private int col = 6;
+        private int row = 5;
+
+        public Lab_2_6(){
+            items = new int[col, row];
+        }
         public override void Do(){
+            Utils.FillArray(this.items);
+            Console.WriteLine("Source values: ");
+            Utils.PrintArray(this.items);
+            Console.WriteLine();
+
+            if (row % 2 == 0) {
+               throw new Exception(Message);
+            }
+
+            for(int i = 0; i < row /2; i++){
+                for(int j = 0; j < col; j++){
+                    int tmp = items[j, i];
+                    items[j, i] = items[j, row - 1 - i];
+                    items[j, row - 1 - i] = tmp;
+                }
+            }
+
+            Console.WriteLine("Source values: ");
+            Utils.PrintArray(this.items);
             Console.WriteLine();
         }
 
