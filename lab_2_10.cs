75bd270c7e757c5d65496daa2d5317d77619c5c4
OloloevReal <nickey.n@gmail.com>
2021-09-01 20:36:12 +0300

    Lab_2_10 done

--- lab_2_10.cs
+++ lab_2_10.cs
@@ -3,19 +3,44 @@ using System;
 namespace Labs_2
 {
     class Lab_2_10: ILab{
-        private int[,] items;
+        private float[,] items;
         private const int col = 5;
         private const int row  = 6;
         public Lab_2_10(){
-            this.items = new int[col,row];
+            this.items = new float[col,row];
         }
         public override void Do(){
-            // Utils.FillArray(this.items);
-            // Console.WriteLine("Source values: ");
-            // Utils.PrintArray(this.items);
-            // Console.WriteLine();
+            Utils.FillArray(this.items);
+            Console.WriteLine("Source values: ");
+            Utils.PrintArray(this.items);
+            Console.WriteLine();
 
+            float tmpValue = items[0, 0];
+            int maxColId = 0;
+            for(int i = 0; i < col; i++){
+                for(int j = 0; j < row; j++){
+                    if(tmpValue < items[i, j]){
+                        tmpValue = items[i, j];
+                        maxColId = i;
+                    }
+                }
+            }
 
+            float[,] tmpItems = new float[col - 1, row];
+            for(int i = 0; i < col; i++){
+                for(int j = 0; j < row; j++){
+                    if(i < maxColId){
+                        tmpItems[i, j] = items[i, j];
+                    }else if(i > maxColId){
+                        tmpItems[i - 1, j] = items[i, j];
+                    }
+                }
+            }
+
+            items = tmpItems;
+            Console.WriteLine("Max element: {0}, column id: {1}", tmpValue, maxColId);
+            Console.WriteLine("Result values: ");
+            Utils.PrintArray(items);
             Console.WriteLine();
         }
 
